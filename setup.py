e9c732d6c04d56e9d5d7aab4b1e081342afbc460
winlu <derwinlu+git@gmail.com>
2014-04-06 09:20:07 +0200

    new structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name='python-twitch',
+	version='0.1',
+	packages=['python-twitch'],
+	license=open('LICENSE').read(),
+	long_description=open('README.md').read(),
+	author='winlu',
+	author_email='derwinlu+python-twitch@gmail.com',
+)
+
