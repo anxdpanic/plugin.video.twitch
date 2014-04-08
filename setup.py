6c8211e600e991c6d7ae3e0313b587c9b0042735
winlu <derwinlu+git@gmail.com>
2014-04-08 11:36:34 +0200

    fixed manifest.in so setuptools can install

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-	name='python-twitch',
+    name='python-twitch',
     description='Python Module for interaction with twitch.tv',
-	version='0.1.2',
-	packages=['twitch'],
-	license=open('LICENSE.txt').read(),
-	long_description=open('README.md').read(),
-	author='winlu',
-	author_email='derwinlu+python-twitch@gmail.com',
+    version='0.1.3',
+    packages=['twitch'],
+    license=open('LICENSE.txt').read(),
+    long_description=open('README.md').read(),
+    author='winlu',
+    author_email='derwinlu+python-twitch@gmail.com',
     url='https://pypi.python.org/pypi/python-twitch',
 )
 
