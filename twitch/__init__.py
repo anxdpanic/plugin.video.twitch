4b8cc92ce3c22f5c774088d3354fae99c1cb2ec1
derwinlu <derwinlu+git@gmail.com>
2015-10-01 19:22:11 +0200

    remove old python-twitch implementation

--- twitch/__init__.py
+++ twitch/__init__.py
@@ -1,13 +1,3 @@
 # -*- encoding: utf-8 -*-
 
-from twitch.app import Twitch
-from twitch.exceptions import TwitchException  # NOQA
-from twitch.logging import deprecation_warning, log
-
-__all__ = ["Twitch", "TwitchException"]
 VERSION = '0.5.0'
-
-
-def TwitchTV(logger=log):
-    deprecation_warning(logger, 'TwitchTV', 'Twitch')
-    return Twitch(logger)
