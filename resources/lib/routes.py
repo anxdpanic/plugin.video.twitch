308a98934669c2fc245e7c573ff3ccfe92a80a50
anxdpanic <anxdpanic@users.noreply.github.com>
2017-03-30 17:45:42 -0400

    add toggle for blacklist confirmation

--- resources/lib/routes.py
+++ resources/lib/routes.py
@@ -548,12 +548,14 @@ def edit_user_blocks(target_id, name):
 def edit_blacklist(list_type='user', target_id=None, name=None, remove=False):
     if not remove:
         if not target_id or not name: return
-
-        confirmed = kodi.Dialog().yesno(i18n('blacklist'), i18n('confirm_blacklist') % name)
-        if confirmed:
+        if kodi.get_setting('blacklist_confirm_toggle') == 'true':
+            confirmed = kodi.Dialog().yesno(i18n('blacklist'), i18n('confirm_blacklist') % name)
+            if confirmed:
+                result = utils.add_blacklist(target_id, name, list_type)
+                if result:
+                    kodi.notify(msg=i18n('blacklisted') % name, sound=False)
+        else:
             result = utils.add_blacklist(target_id, name, list_type)
-            if result:
-                kodi.notify(msg=i18n('blacklisted') % name, sound=False)
     else:
         result = utils.remove_blacklist(list_type)
         if result:
