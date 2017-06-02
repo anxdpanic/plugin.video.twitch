44824f356720ad37e3cd83c2c2cf5ea012777e97
anxdpanic <anxdpanic@users.noreply.github.com>
2017-06-02 11:14:29 -0400

    don't ask for search query from '..' (#310)

--- resources/lib/routes.py
+++ resources/lib/routes.py
@@ -178,11 +178,14 @@ def search():
 @dispatcher.register(MODES.NEWSEARCH, args=['content'])
 @error_handler
 def new_search(content):
-    kodi.set_view('files', set_sort=False)
-    user_input = kodi.get_keyboard(i18n('search'))
-    if user_input:
-        kodi.end_of_directory()
-        kodi.update_container(kodi.get_plugin_url({'mode': MODES.SEARCHRESULTS, 'content': content, 'query': user_input, 'index': 0}))
+    if MODES.SEARCHRESULTS not in kodi.get_info_label('Container.FolderPath'):
+        kodi.set_view('files', set_sort=False)
+        user_input = kodi.get_keyboard(i18n('search'))
+        if user_input:
+            kodi.end_of_directory()
+            kodi.update_container(kodi.get_plugin_url({'mode': MODES.SEARCHRESULTS, 'content': content, 'query': user_input, 'index': 0}))
+        else:
+            return
     else:
         return
 
