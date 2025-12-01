34fd0763f46265e424267b3262a2a821d27e8786
Seraph91P <github@mebert-server.de>
2025-12-01 16:37:09 +0100

    Add detailed exception messages for Twitch-related errors

--- resources/lib/twitch_addon/addon/twitch_exceptions.py
+++ resources/lib/twitch_addon/addon/twitch_exceptions.py
@@ -18,12 +18,40 @@ class TwitchException(Exception):
 
 
 class SubRequired(Exception):
+    """Raised when subscription is required to view content"""
     pass
 
 
 class NotFound(Exception):
+    """Raised when requested content is not found"""
     pass
 
 
 class PlaybackFailed(Exception):
+    """Raised when video playback fails"""
+    pass
+
+
+class StreamOffline(Exception):
+    """Raised when trying to watch a stream that is offline"""
+    pass
+
+
+class TokenExpired(Exception):
+    """Raised when OAuth token has expired"""
+    pass
+
+
+class RateLimited(Exception):
+    """Raised when API rate limit is exceeded"""
+    pass
+
+
+class NetworkError(Exception):
+    """Raised when network connection fails"""
+    pass
+
+
+class QualityUnavailable(Exception):
+    """Raised when requested video quality is not available"""
     pass
